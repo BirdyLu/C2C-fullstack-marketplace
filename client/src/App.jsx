7ec78f1034fa05330ba318678848f82b3f438adf
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-12-06 14:42:42 +0800

    create ages and Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import  {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 
 export default function App() {
   return (
-    <div className='text-red-600'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
